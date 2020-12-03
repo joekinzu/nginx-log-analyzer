ac864d2b0bed38de57fb4fef52bc5f19e115fe2c
joekinzu <34208764+joekinzu@users.noreply.github.com>
2020-12-03 14:52:07 +0400

    Update log_analyzer.py

--- log_analyzer.py
+++ log_analyzer.py
@@ -9,6 +9,10 @@
 
 import re 
 import time
+import json
+import os
+import glob
+import gzip
 
 CONFIG = {
     "REPORT_SIZE": 1000,
@@ -25,41 +29,37 @@ def perc(d, d1):
     return round_float((d/d1)*100)
 
 # find median
-def median(ar):
-	sorted(ar)
-	n = len(ar)
+def median(ar, n):
 	# if n % 2 == 1:
-	return ar[n//2] if 1>0 else (ar[n//2]+ar[n//2-1])/2
+	return ar[n//2] if n%2==1 else (ar[n//2]+ar[n//2-1])/2
 
 # get lastest filename within folder
 def get_file_date(file_path):
 	file_date = 1
 	return file_path
 
-# get url, request time from nginx log
+# get url, request time from nginx log file
 def parse_data(filename):
 	logdict = {}
 	ctotal = 0
 	btotal = 0
-
-	with open(filename) as f:
+	print('log filename: ', filename)
+	with gzip.open(filename,'rt') as f:
 		for l in f:
 		    data = l.split(' ')
 		    ctotal += 1
 		    btotal += float(l.split(' ')[-1])
-		    logdict[ctotal] = [data[7], float(data[-1])]
-	
-	return sorted(logdict.items(), key=lambda x: x[1]), ctotal, round_float(btotal)
+		    logdict[ctotal] = [data[7], float(data[-1])]	
+	return sorted(logdict.items(), key=lambda x: (x[1],x[0])), ctotal, round_float(btotal)
 
-# write to file
+# write data to json file
 def write_dict_to_file(filename, dict1):
 	with open(filename, 'w') as filename:
-		for s in dict1:
-			filename.write(str(dict1[s]) +"\n")
+		json.dump(dict1, filename)
 
 
 def main():
-	filename = '2.log'
+	filename = max(glob.glob(CONFIG['LOG_DIR']+'/nginx*'),key=os.path.getctime)
 	logdict = {}
 	res = {}
 	ctotal = 0
@@ -74,10 +74,6 @@ def main():
 	logdict, ctotal, btotal = parse_data(filename)
 	print('total rows: ',ctotal)
 	print('total data: ',btotal)
-
-	# with open("bla1.txt", "w") as f:
-	#     for s in logdict:
-	#         f.write(str(s) +"\n")
 
 
 	for s in logdict:
@@ -95,11 +91,11 @@ def main():
 	        tim = tim + s[1][1]
 	        median_array.append(s[1][1])
 	        m = m if m > s[1][1] else s[1][1]
-	        res[count].update(count = cnt, time_sum = round_float(tim), time_avg =  round_float(tim/cnt), time_max = m, count_perc = perc(cnt,ctotal), time_perc =  perc(tim, btotal), time_median = len(median_array))
+	        res[count].update(count = cnt, time_sum = round_float(tim), time_avg =  round_float(tim/cnt), time_max = m, count_perc = perc(cnt,ctotal), time_perc =  perc(tim, btotal), time_median = median(median_array, cnt))
 	 
 	print ('time: ', round_float(time.time()-start))
 
-	# write to file
+	# write results to file
 	write_dict_to_file(CONFIG['REPORT_DIR']+'/'+'result.json', res)
         
         
